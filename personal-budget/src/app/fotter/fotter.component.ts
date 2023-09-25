d3b6a30fd7d9037f03c636350ae7b573d6985cf2
Venkata Lakshman Nimmagadda <lakshman@Venkatas-Air.lan>
2023-09-25 01:31:24 -0400

    completed assignment5

---
+++ personal-budget/src/app/fotter/fotter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pb-fotter',
+  templateUrl: './fotter.component.html',
+  styleUrls: ['./fotter.component.scss']
+})
+export class FotterComponent {
+
+}
